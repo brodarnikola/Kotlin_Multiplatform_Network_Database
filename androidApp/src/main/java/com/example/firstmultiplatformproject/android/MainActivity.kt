654ad2b7df0e134a4828bf4010d0970bcbd947fb
brodarnikola <brodarnikola@gmail.com>
2021-04-24 14:21:08 +0200

    Added scroll to top with smoothScroller

--- androidApp/src/main/java/com/example/firstmultiplatformproject/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/firstmultiplatformproject/android/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.firstmultiplatformproject.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
-import com.example.firstmultiplatformproject.shared.Greeting
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.firstmultiplatformproject.shared.Greeting
 import com.example.firstmultiplatformproject.shared.SpaceXSDK
 import com.example.firstmultiplatformproject.shared.cache.DatabaseDriverFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+
 
 fun greet(): String {
     return Greeting().greeting()
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private val mainScope = MainScope()
 
+    private lateinit var scrollToTop: FloatingActionButton
     private lateinit var launchesRecyclerView: RecyclerView
     private lateinit var progressBarView: FrameLayout
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
@@ -35,13 +40,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         title = "SpaceX Launches"
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        initializeUi()
+    }
+
+    private fun initializeUi() {
 
         launchesRecyclerView = findViewById(R.id.launchesListRv)
         progressBarView = findViewById(R.id.progressBar)
         swipeRefreshLayout = findViewById(R.id.swipeContainer)
+        scrollToTop = findViewById(R.id.scrollToTop)
 
         launchesRecyclerView.adapter = launchesRvAdapter
         launchesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        val smoothScroller: SmoothScroller = object : LinearSmoothScroller(this@MainActivity) {
+            override fun getVerticalSnapPreference(): Int {
+                return SNAP_TO_START
+            }
+        }
+
+        scrollToTop.setOnClickListener {
+            smoothScroller.setTargetPosition(0)
+            (launchesRecyclerView.layoutManager as LinearLayoutManager).startSmoothScroll(smoothScroller)
+        }
 
         swipeRefreshLayout.setOnRefreshListener {
             swipeRefreshLayout.isRefreshing = false
